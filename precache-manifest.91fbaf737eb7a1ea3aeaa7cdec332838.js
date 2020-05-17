3412179e15b93e4b78de10b3725d39779e631daf
Ghore <ghoreua@gmail.com>
2020-05-17 11:18:20 +0300

    Updates

---
+++ precache-manifest.91fbaf737eb7a1ea3aeaa7cdec332838.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3fc214c3b079f67ffcd28b16d1b646",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "4f8b73f223053ca1fe29",
+    "url": "/goit-react-hw-02-feedback/static/js/2.b941e8f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.b941e8f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14a90a6efcfb8d94076",
+    "url": "/goit-react-hw-02-feedback/static/js/main.dcdcc68e.chunk.js"
+  },
+  {
+    "revision": "cedbd5c2db3fabd13fa5",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.5b3cea23.js"
+  }
+]);
